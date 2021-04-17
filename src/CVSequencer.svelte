ad90f39b6b99039593db6dd786eb7a5312493539
Sean McCullough <banksean@gmail.com>
2021-04-16 22:53:21 -0700

    ¯\_(ツ)_/¯

---
+++ src/CVSequencer.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+    import { Faceplate, Knob, Patch, Bang } from '@patchcab/core';
+    import { Signal, } from 'Tone';
+    import Indicator from './Indicator.svelte';
+ 
+    const STEPS = 4;
+  
+    export let state = {
+      seqSteps: Array(STEPS).fill(0)
+    };
+  
+    let currentStep = 0;
+  
+    const cvOut = new Signal();
+
+    const resetIn = new Bang((time, on, off) => {
+        currentStep = 0;
+    }); 
+
+    const advanceIn = new Bang((time, on, off) => {
+        currentStep++;
+        currentStep = currentStep % STEPS;
+        let val = state.seqSteps[currentStep];
+        cvOut.setValueAtTime(val, time);
+    });
+  </script>
+  
+  <style>
+    steps {
+    }  
+  </style>
+  
+  <Faceplate title="CVSEQ" color="#1D1E22">
+      <steps style="top:40px">
+        {#each state.seqSteps as cv, stepIndex}
+            <Knob size="s"
+              x={20}
+              y={stepIndex * 48 + 60} 
+              bind:value={state.seqSteps[stepIndex]}
+              min={0}
+              max={1.0}
+              precision={2}
+            >
+          </Knob>
+          <Indicator x={68} y={stepIndex * 48 + 76} active={stepIndex == currentStep}></Indicator>
+        {/each}
+      </steps>
+    <Patch label="out" x={62} y={48 + STEPS * 64} name="out-cv" output={cvOut} />
+    <Patch label="adv" x={18} y={STEPS * 64} name="advance" input={advanceIn} />
+    <Patch label="rst" x={62} y={STEPS * 64} name="reset" input={resetIn} />
+  </Faceplate>
+  
---
+++ src/Indicator.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import { Label } from '@patchcab/core';
+
+    export let label = undefined;
+    export let high: boolean;
+    export let active = false;
+    export let x = 0;
+    export let y = 0;
+ </script>
+  
+  <style>
+    button {
+      position: absolute;
+      width: 18px;
+      height: 18px;
+      border-radius: 18px;
+      background: var(--color-dark);
+      border: 1px solid rgba(0, 0, 0, 0.32); 
+      box-shadow: inset 2px 2px 0px rgba(255, 255, 255, 0.08), 0px 0px 0px 2px rgba(0, 0, 0, 0.12);
+      cursor: pointer;
+      color: inherit;
+    }
+    button.high {
+      background: var(--color-dark-highlight);
+    }
+    button {
+      width: 10px;
+      height: 10px;
+      border-radius: 50%;
+    }
+    button.on {
+      background: var(--color-on);
+      box-shadow: inset 2px 2px 0px rgba(255, 255, 255, 0.24), 0px 0px 0px 2px rgba(0, 0, 0, 0.12);
+    }
+    button.on.high {
+      background: var(--color-on-highlight);
+    }
+  </style>
+
+<button
+aria-label="indicator"
+style="left: {x}px; top: {y}px;"
+class:on={active}
+class:high>
+{#if label}
+<Label top={-1}>{label}</Label>
+{/if}
+</button>
